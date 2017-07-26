1884d244b44a83f2cf7ff70b2b262da16e6e62e9
Hilary Egan <hilary.egan@colorado.edu>
2017-07-26 10:05:45 -0400

    starting to iterate through pixels

--- main.cpp
+++ main.cpp
@@ -5,7 +5,9 @@
 int main()
 {
   double center_x, center_y, length_x, length_y, min_x, min_y, pixel_size;
+  double x, y;
   int pixel_count_x, pixel_count_y;
+  int i_x, i_y, i;
   int * pixels;
  
   
@@ -20,4 +22,30 @@ int main()
   pixel_count_y = length_y/pixel_size;
     
   pixels = new int[pixel_count_x*pixel_count_y];
+  
+
+  for (i_x=0; i_x<pixel_count_x; i_x++){
+      for (i_y=0; i_y<pixel_count_y; i_y++){
+
+          x = min_x + pixel_size_x*i_x;
+          y = max_y - pixel_size*i_y;
+
+          i = i_y*pixel_count_x+i_x;
+          pixels[i] = Mandelbrot(x,y);
+
+      }
+  }
+
+
+}
+
+int Mandelbrot(double x, double y){
+    int iter, iter_max=1000;
+    double radius_z, radius_max=2.0;
+
+    while ((radius_x < radius_max) && (iter < iter_max)){
+
+
+    }
+
 }
