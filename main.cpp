58e6804ec664383b61ab0fb085ff0eb906cf5c9a
eganhila <hilaryye@gmail.com>
2017-07-26 09:50:26 -0400

    creating main.cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+//Main
+
+#include<iostream>
+
+int main()
+{
+  double center_x, center_y, length_x, length_y, min_x, min_y, pixel_size;
+  int pixel_count_x, pixel_count_y;
+  
+  center_x = -0.75;
+  center_y = 0.00;
+  length_x = 2.75;
+  length_y = 2.0;
+  min_x = center_x -length_x/2.0;
+  max_y = center_y + length_y/2.0;
+  pixel_count_x = 8192;
+  pixel_size = length_x/pixel_count_x;
+  pixel_count_y = length_y/pixel_size
+}
