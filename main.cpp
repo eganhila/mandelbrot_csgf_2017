649d8498962eedd4418cf58663f2640b88a80d4b
eganhila <hilaryye@gmail.com>
2017-07-26 09:54:03 -0400

    Allocate memory

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@ int main()
 {
   double center_x, center_y, length_x, length_y, min_x, min_y, pixel_size;
   int pixel_count_x, pixel_count_y;
+  int * pixels;
+ 
   
   center_x = -0.75;
   center_y = 0.00;
@@ -15,5 +17,7 @@ int main()
   max_y = center_y + length_y/2.0;
   pixel_count_x = 8192;
   pixel_size = length_x/pixel_count_x;
-  pixel_count_y = length_y/pixel_size
+  pixel_count_y = length_y/pixel_size;
+    
+  pixels = new int[pixel_count_x*pixel_count_y];
 }
